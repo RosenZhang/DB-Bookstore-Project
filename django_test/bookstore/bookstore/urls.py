5f71cf742ef8a0b09df6bc33d25184b6daf0c617
pappar1027 <zou_yun@mymail.sutd.edu.sg>
2017-11-14 14:06:33 +0800

    change projectname

---
+++ django_test/bookstore/bookstore/urls.py
@@ -0,0 +1,44 @@
+"""mytestsite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
+# Use static() to add url mapping to serve static files (like CSS, JS and images by default) during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    url(r'^catalog/', include('catalog.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    # url(r'^signup/$', catalog_views.signup, name='signup'),
+    # for sign up
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
