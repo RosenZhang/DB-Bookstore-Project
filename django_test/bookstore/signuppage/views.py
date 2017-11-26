e4c424f7d107795f2592196ac6add2613ec77caf
Jinglianwu <ntjinglian@gmail.com>
2017-11-26 17:38:27 +0800

    add more on signup. url, html

--- django_test/bookstore/signuppage/views.py
+++ django_test/bookstore/signuppage/views.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
 from django.shortcuts import render
 
