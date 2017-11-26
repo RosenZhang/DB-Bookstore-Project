5991fda9dd2e368ba8fe99d5371ba768626fc951
pappar1027 <zou_yun@mymail.sutd.edu.sg>
2017-11-26 16:30:17 +0800

    change database, tidy up html

--- django_test/bookstore/bookstore/settings.py
+++ django_test/bookstore/bookstore/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
-    'storemanager.apps.StoremanagerConfig',
 ]
 
 MIDDLEWARE = [
