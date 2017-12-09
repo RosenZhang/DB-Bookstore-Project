995b824e2ed9a3abe4d939b11f4970ccb646cf22
Elaine Cheong <elainecheong@Admins-MacBook-Pro.local>
2017-12-09 20:42:12 +0800

    added tables for most popular titles, authors, publisher

--- django_test/mytestsite/catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    # a function in views called index will be called. ^s start marker, $ is end marker
-    # name is to idenfy the mapping
-]
