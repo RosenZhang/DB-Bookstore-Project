d95ca563c374a76078be43b32f09419c73a8d224
RosenZhang <RosenZhang@users.noreply.github.com>
2017-12-09 20:34:40 +0800

    Update bookstoremysqlsetup.sql

--- bookstoremysqlsetup.sql
+++ bookstoremysqlsetup.sql
@@ -2,6 +2,6 @@ CREATE DATABASE dbproject CHARACTER SET UTF8;
 
 CREATE USER DBprojectUser@localhost IDENTIFIED BY 'password';
 
-GRANT ALL PRIVILEGES ON DBproject.* TO DBprojectUser@localhost;
+GRANT ALL PRIVILEGES ON dbproject.* TO DBprojectUser@localhost;
 
 FLUSH PRIVILEGES;
