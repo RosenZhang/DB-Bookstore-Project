70cac3bbb738962d43e31018e71d1a72605e7f76
RosenZhang <rosenzhang97@gmail.com>
2017-12-10 13:56:50 +0800

    change card view

--- django_test/bookstore/userbook/urls.py
+++ django_test/bookstore/userbook/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import usermainpage_view, book_view,recommendation_view
 
 urlpatterns = [
-	url(r'^$', usermainpage_view,name='main'),
-	url(r'^(?P<ISBN13>\d*-\d*)/(?P<topnum>\d*)orderconfirmed$',recommendation_view),
-	url(r'^(?P<ISBN13>\d*-\d*)/(?P<topnum>\d*)$',book_view),
+    url(r'^$', usermainpage_view,name='main'),
+    url(r'^(?P<ISBN13>\d*-\d*)/(?P<topnum>\d*)orderconfirmed$',recommendation_view),
+    url(r'^(?P<ISBN13>\d*-\d*)/(?P<topnum>\d*)$',book_view),
 ]
